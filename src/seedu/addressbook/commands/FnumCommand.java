701f010e9654a3b62bf4112f3d20fae405d1c312
Daniel Pyone Maung <c.danpm@gmail.com>
2019-02-12 17:39:31 +0800

    add fnum in parser

--- src/seedu/addressbook/commands/FnumCommand.java
+++ src/seedu/addressbook/commands/FnumCommand.java
@@ -1,4 +1,61 @@
 package seedu.addressbook.commands;
 
-public class FnumCommand {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+/**
+ * Finds and lists all persons in address book whose number contains any of the argument keywords.
+ */
+
+public class FnumCommand extends Command {
+
+    public static final String COMMAND_WORD = "fnum";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose number contains the numbers searched and "
+            + "displays them as a list in an index manner. Put spaces between different phone numbers to be searched.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " 081808 133 00";
+
+    private final Set<String> keywords;
+
+    public FnumCommand(Set<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    /**
+     * Returns a copy of keywords in this command.
+     */
+    public Set<String> getKeywords() {
+        return new HashSet<>(keywords);
+    }
+
+    @Override
+    public CommandResult execute() {
+        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
+        return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
+    }
+
+    /**
+     * NEED TO WORK ON THE STUFF BELOW TOOO!!!
+     * Retrieves all persons in the address book whose names contain some of the specified keywords.
+     *
+     * @param keywords for searching
+     * @return list of persons found
+     */
+    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
+        final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        for (ReadOnlyPerson person : addressBook.getAllPersons()) {
+            final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
+            if (!Collections.disjoint(wordsInName, keywords)) {
+                matchedPersons.add(person);
+            }
+        }
+        return matchedPersons;
+    }
+
 }
